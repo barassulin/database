e70cddcc9e992add8e37f529a88d42619144ebe1
barassulin <148526209+barassulin@users.noreply.github.com>
2024-11-11 13:27:34 +0200

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -1,47 +1,121 @@
+import threading
+import time
+
 import synced_database
 import os
 
 
 class Tests:
-    FILE_PATH = 'TEST'
 
-    def __init__(self, test_dict, num_of):
+    def __init__(self, test_dict, mode):
         self.test_dict = test_dict
-        self.num_of = num_of
+        self.mode = mode
+
+    #@staticmethod
+    def func(self, f5, arg):
+        print("func")
+
+    def locki(self, thing):
+        if self.mode == 'threads':
+            if isinstance(thing, threading.Semaphore):
+                v = not thing.acquire(blocking=False)
+                if not v:
+                    thing.release()
+            else:
+                v = thing.locked()
+
+        elif self.mode == 'processes':
+            v = not thing.acquire(block=False)
+            if not v:
+                thing.release()
+        else:
+            v = 'err'
+        return v
 
     def create_file(self, file_path):
         # create file w test_dict
-        db = synced_database.SyncedDatabase(file_path, 'mode')
-        return db.dict_set(self.test_dict)
+        db = synced_database.SyncedDatabase(file_path, self.mode)
+        print(db.dict_set(self.test_dict))
+        return db
 
-    def delete_file(self, file_path):
+    @staticmethod
+    def delete_file(file_path):
         # delete file
-        if os.path.exists(self, file_path):
+        if os.path.exists(file_path):
             os.remove(file_path)
+        print(not os.path.exists(file_path))
+
+    def check_multread_write(self, sdb, key, val):
+        things = []
+        count = 0
+        valu = True
+        while count < sdb.max:
+            count = count + 1
+            if self.locki(sdb.read_semaphore) or sdb.get_val_of_sem() >= sdb.max:
+                valu = False
+            thing = self.func(sdb.value_get, [key])
+            things.append(thing)
+        v = sdb.value_set(key, val)
+        print(valu)
+        return things
+
+    def check_multread(self, sdb, key):
+        things = []
+        count = 0
+        val = True
+        while count < sdb.max:
+            count = count+1
+            if self.locki(sdb.read_semaphore):
+                val = False
+            thing = self.func(sdb.value_get, [key])
+            things.append(thing)
+        print(val)
+        return things
+
+    def read_no_write(self, sdb, key, val):
+        value = False
+        thing = self.func(sdb.value_get, [key])
+        time.sleep(1)
+        # if not self.locki(sdb.read_semaphore):
+        #     while not self.locki(sdb.read_semaphore):
+        #         continue
+        if sdb.get_val_of_sem() < sdb.max:
+            value = True
+        print(value)
+        return [thing]
+
+    def write_no_read(self, sdb, key, val):
+        value = False
+        thing = self.func(sdb.value_set, [key, val])
+        time.sleep(1)
+        if self.locki(sdb.read_semaphore):
+            value = True
+        print(value)
+        return [thing]
+
+    # @staticmethod
+    def write_per(self, sdb, key, val):
+        # write wo comp & read wo comp
+        value = False
+        if not self.locki(sdb.write_lock):
+            if sdb.value_set(key, val) and val == sdb.value_get(key):
+                if not self.locki(sdb.write_lock):
+                    value = True
+        return value
+
+    def read_per(self, sdb, key, val):
+        # write wo comp & read wo comp
+        value = False
+        sdb.value_set(key, val)
+        if not self.locki(sdb.read_lock):
+            if val == sdb.value_get(key):
+                if not self.locki(sdb.read_lock):
+                    value = True
+        return value
 
     @staticmethod
-    def write_read(db, key, val):
-        # write and read to see if worked
-        db.value_set(key, val)
-        value = db.value_get(key)
-        return value == val
-
-    @staticmethod
-    def delete_read(db, key):
-        # delete and read to see if worked
-        db.value_delete(key)
-        return key in dict.keys()
-
-    def read_mult(self, func):
-        # read multi=num_of
-        counter = 0
-        while counter != self.num_of:
-            func()
-            counter = counter + 1
-
-    # def run_all(self, func):
-        # run all
-# ASSERTS:
-#         if os.path.exists(self.file_path):
-#             return False
-#         return True
+    def delete_read(sdb, key):
+        # delete wo comp
+        val = sdb.value_get(key)
+        val2 = sdb.value_delete(key)
+        return key not in sdb.dict_keys() and val == val2
